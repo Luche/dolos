6a4b4f73fe745d302ed23dddb6d6545a930b8732
Luche <henrylucky68@gmail.com>
2026-04-06 09:49:22 +0700

    Fixed http redirect in nginx->Rails

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  def default_url_options
+    if Rails.configuration.respond_to?(:dolos_api_url)
+      { protocol: Rails.configuration.dolos_api_url.scheme }
+    else
+      {}
+    end
+  end
 end
